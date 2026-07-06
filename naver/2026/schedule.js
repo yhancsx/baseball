b294d85c10a52791811f332e2b7c207735cb9605
yhan-bae <yhan.bae@navercorp.com>
2026-07-06 09:35:30 +0900

    feat: naver 디렉토리 및 페이지 생성, 포털 랜딩 페이지 추가

---
+++ naver/2026/schedule.js
@@ -0,0 +1,90 @@
+const scheduleData = [
+    {
+        "id": 1,
+        "date": "02월23일(월) 20:30",
+        "category": "유신 평일야간리그 (유신고등학교 야구장)",
+        "match": "더이상은 NAVER vs R.O.K 어벤져스",
+        "result": "패",
+        "score": "1:14"
+    },
+    {
+        "id": 2,
+        "date": "03월20일(금) 20:30",
+        "category": "유신 평일야간리그 (유신고등학교 야구장)",
+        "match": "영흥숲BC vs 더이상은 NAVER",
+        "result": "승",
+        "score": "12:11"
+    },
+    {
+        "id": 3,
+        "date": "03월26일(목) 20:30",
+        "category": "유신 평일야간리그 (유신고등학교 야구장)",
+        "match": "Mz뽀로로 vs 더이상은 NAVER",
+        "result": "패",
+        "score": "11:13"
+    },
+    {
+        "id": 4,
+        "date": "04월06일(월) 20:30",
+        "category": "유신 평일야간리그 (유신고등학교 야구장)",
+        "match": "더이상은 NAVER vs 알파 브레이브스",
+        "result": "패",
+        "score": "8:18"
+    },
+    {
+        "id": 5,
+        "date": "05월07일(목) 20:30",
+        "category": "유신 평일야간리그 (유신고등학교 야구장)",
+        "match": "더이상은 NAVER vs 우리끼리",
+        "result": "승",
+        "score": "16:2"
+    },
+    {
+        "id": 6,
+        "date": "05월22일(금) 20:30",
+        "category": "유신 평일야간리그 (유신고등학교 야구장)",
+        "match": "한전KDN hubs vs 더이상은 NAVER",
+        "result": "승",
+        "score": "16:8"
+    },
+    {
+        "id": 7,
+        "date": "06월12일(금) 20:30",
+        "category": "유신 평일야간리그 (유신고등학교 야구장)",
+        "match": "더이상은 NAVER vs 라온제나",
+        "result": "패",
+        "score": "6:8"
+    },
+    {
+        "id": 8,
+        "date": "06월17일(수) 20:30",
+        "category": "유신 평일야간리그 (유신고등학교 야구장)",
+        "match": "더이상은 NAVER vs MAJOR",
+        "result": "패",
+        "score": "9:11"
+    },
+    {
+        "id": 9,
+        "date": "07월01일(수) 20:30",
+        "category": "유신 평일야간리그 (유신고등학교 야구장)",
+        "match": "국민낙곱새 vs 더이상은 NAVER",
+        "result": "패",
+        "score": "9:16"
+    },
+    {
+        "id": 10,
+        "date": "07월21일(화) 20:30",
+        "category": "유신 평일야간리그 (유신고등학교 야구장)",
+        "match": "더이상은 NAVER vs Mz뽀로로",
+        "result": null,
+        "score": null
+    },
+    {
+        "id": 11,
+        "date": "07월27일(월) 20:30",
+        "category": "유신 평일야간리그 (유신고등학교 야구장)",
+        "match": "R.O.K 어벤져스 vs 더이상은 NAVER",
+        "result": null,
+        "score": null
+    }
+];
